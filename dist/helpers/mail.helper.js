74d3094d0a063c9efc87f02ad96a07422dafccf3
Artem <artem.volcano@gmail.com>
2025-03-24 04:39:54 +0500

    fgdh

--- dist/helpers/mail.helper.js
+++ dist/helpers/mail.helper.js
@@ -18,11 +18,20 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const sendEmail = (email, topic, message) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(email, topic, message, String(process.env.MAIL_USER), String(process.env.MAIL_PASSWORD));
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail", // or other services like 'yahoo', 'outlook'
+    //   auth: {
+    //     user: String(process.env.MAIL_USER),
+    //     pass: String(process.env.MAIL_PASSWORD), // https://myaccount.google.com/apppasswords?pli=1&rapt=AEjHL4PpOEhFWSLQSBlGFN2GGdelEusmBXNcKRAUlNbqOKjxNkbm5tblyXeehDi_boDUCGm4jhoQ1MKuqnUb2gykr7hJu2fCKsxR1ULDrmJB0bCF79NkgbE (if 2-step autentification is on)
+    //   },
+    // });
     const transporter = nodemailer_1.default.createTransport({
-        service: "gmail", // or other services like 'yahoo', 'outlook'
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: String(process.env.MAIL_USER),
-            pass: String(process.env.MAIL_PASSWORD), // https://myaccount.google.com/apppasswords?pli=1&rapt=AEjHL4PpOEhFWSLQSBlGFN2GGdelEusmBXNcKRAUlNbqOKjxNkbm5tblyXeehDi_boDUCGm4jhoQ1MKuqnUb2gykr7hJu2fCKsxR1ULDrmJB0bCF79NkgbE (if 2-step autentification is on)
+            pass: String(process.env.MAIL_PASSWORD),
         },
     });
     const mailOptions = {
